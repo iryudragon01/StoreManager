48143e3700b7227411c7a7d1719c4cfbff86d633
BANDAHN NAMBOONMA <iryudragon01@gmail.com>
2019-11-30 17:38:15 +0700

    list timezone step 2

--- stock/data2view/store/sale/forms.py
+++ stock/data2view/store/sale/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from stock.models import Sale
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from django.utils import timezone
 
+
 class ItemForm(forms.Form):
-    name= forms.TextInput()
+    name = forms.TextInput()
     start = forms.IntegerField()
     now = forms.IntegerField()
-    sale=forms.IntegerField()
+    sale = forms.IntegerField()
     price = forms.IntegerField()
     money = forms.IntegerField()
     sum = forms.IntegerField()
@@ -15,33 +16,20 @@ class ItemForm(forms.Form):
 
 class SaleForm(forms.ModelForm):
     class Meta:
-        model = Sale    
+        model = Sale
         fields = ['id']
 
 
 class ListTime(forms.Form):
-    getDate=forms.CharField(max_length=200,widget=forms.DateTimeInput(
-        attrs={'type':'datetime-local'}
+    getDate = forms.CharField(max_length=200, widget=forms.DateTimeInput(
+        attrs={'type': 'datetime-local'}
     ))
 
     def clean_getDate(self):
         getdate = self.cleaned_data['getDate']
-        diff=timediff()
         try:
-           rawtime = datetime.strptime(getdate,'%Y-%m-%dT%H:%M')+\
-                     timedelta(hours=diff['hour'],minutes=diff['minute'])
-           return rawtime
+            time = datetime.strptime(getdate, '%Y-%m-%dT%H:%M')
+            return time
 
         except:
             raise forms.ValidationError('format date is not correct')
-
-
-def timediff() :
-    timeZ = timezone.now()
-    timeD = datetime.now()
-    subz = 24*60*timeZ.day+60*timeZ.hour+timeZ.minute
-    subd = 24*60*timeD.day+60*timeD.hour+timeD.minute
-    diff = subz-subd
-    hour = int(diff / 60)
-    minute =diff-hour*60
-    return {'hour':hour,'minute':minute}
--- stock/data2view/store/sale/views.py
+++ stock/data2view/store/sale/views.py
@@ -1,116 +1,121 @@
-from django.shortcuts import render,redirect,Http404,HttpResponse
-from stock.models import Sale,Item,Worker
-from stock.data2view.user import action,queries
-from . import forms,ajax
+from django.shortcuts import render, redirect, Http404, HttpResponse
+from stock.models import Sale, Item, Worker
+from stock.data2view.user import action, queries
+from . import forms, ajax
 from django.db.models import Sum
-from django.utils import timezone
-from django.utils.timezone import get_current_timezone_name
 import json
 from datetime import datetime
-import pytz
+from django.utils import timezone
 
-def IndexView(request,url):
+
+def IndexView(request, url):
     content = {}
     worker = queries.get_worker(url, username=request.session['worker'])
     if request.POST:
         if request.is_ajax():
-            data = json.dumps(ajax.SaleEnableButton(request,worker))
-            return HttpResponse(data,content_type='application/json')
+            data = json.dumps(ajax.SaleEnableButton(request, worker))
+            return HttpResponse(data, content_type='application/json')
     items = Item.objects.filter(
-        user=worker.supervisor )
+        user=worker.supervisor)
 
-    form_list=[]
+    form_list = []
     if items.exists():
         content['items'] = items
         for item in items:
-            form_list.append(salelist(request,item))
+            form_list.append(salelist(request, item))
         content['forms'] = form_list
     else:
-        content['message']   = 'you donot have any item'
-    BTNDisplay= "Enable" if worker.enable_sale else "Disable"
-    BTNclass= "btn-outline-success" if worker.enable_sale else "btn-outline-secondary"
-    BTNValue =  1 if worker.enable_sale else 0
-    EnableBTN={'label':BTNDisplay,'value':BTNValue,'class':BTNclass}
-    content['enableBTN']=EnableBTN
+        content['message'] = 'you donot have any item'
+    BTNDisplay = "Enable" if worker.enable_sale else "Disable"
+    BTNclass = "btn-outline-success" if worker.enable_sale else "btn-outline-secondary"
+    BTNValue = 1 if worker.enable_sale else 0
+    EnableBTN = {'label': BTNDisplay, 'value': BTNValue, 'class': BTNclass}
+    content['enableBTN'] = EnableBTN
     worker.save()
-    return render(request, 'stock/store/sale/index.html',content)
+    return render(request, 'stock/store/sale/index.html', content)
 
 
-def DetailView(request,url,pk):
+def DetailView(request, url, pk):
     content = {}
-    worker =queries.get_worker(url,request.session['worker'])
-    item= Item.objects.get(id=pk)
+    worker = queries.get_worker(url, request.session['worker'])
+    item = Item.objects.get(id=pk)
     if item:
         sale_1 = Sale.objects.filter(
-                    item=item,
-                    creater_id=worker.id,
-                    create_time__gt=worker.date_log,
-                    create_time__lte=timezone.now()
-                    )
+            item=item,
+            creater_id=worker.id,
+            create_time__gt=worker.date_log,
+            create_time__lte=datetime.now()
+        )
         if sale_1:
-            content['sales']=sale_1
+            content['sales'] = sale_1
         else:
             print('sale donot exists')
 
+    return render(request, 'stock/store/sale/detail.html', content)
 
-    return render(request,'stock/store/sale/detail.html',content)
 
-def DeleteView(request,url,pk):
+def DeleteView(request, url, pk):
     sale = Sale.objects.filter(id=pk)
     if sale.exists():
-        item_id=sale[0].item.id
+        item_id = sale[0].item.id
         if sale[0].creater_id == queries.get_worker(url=request.session['url'],
-                                    username=request.session['worker']).id:
+                                                    username=request.session['worker']).id:
             delsale = sale[0]
             delsale.delete()
-            return redirect('stock:detail_sale',url=request.session['url'],pk=item_id)
+            return redirect('stock:detail_sale', url=request.session['url'], pk=item_id)
     else:
         return Http404
 
-def ListView(request,url):
+
+def ListView(request, url):
     content = {}
-    worker = queries.get_worker(url,username=request.session['worker'])
+    worker = queries.get_worker(url, username=request.session['worker'])
     start_time = worker.date_log
     form = forms.ListTime(request.POST or None)
+    creater_id = queries.get_worker(url,username=request.session['worker']).id
     if request.POST:
         if form.is_valid():
-            start_time = form.cleaned_data['getDate']
-    content['form']=form
+            start_time= form.cleaned_data['getDate']
+            creater_id = queries.get_worker(url,pk=request.POST['worker']).id
+    content['form'] = form
     content['workers'] = Worker.objects.filter(supervisor=queries.get_user(request.session['url']))
     sales = Sale.objects.filter(
-        creater_id=worker.id,
+        creater_id=creater_id,
         create_time__gt=start_time
     )
+
     if sales.exists():
-        content['sales']=sales
+        for sale in sales:
+            print(start_time,' ',sale.create_time)
+        content['sales'] = sales
     else:
         content['message'] = 'no sale'
-    return render(request,'stock/store/sale/list.html',content)
+    return render(request, 'stock/store/sale/list.html', content)
 
 
-def AjaxSaleView(request,url):
+def AjaxSaleView(request, url):
     if request.POST:
         if request.is_ajax():
-            worker = queries.get_worker(url,username=request.session['worker'])
+            worker = queries.get_worker(url, username=request.session['worker'])
             if not worker.enable_sale:
-                return HttpResponse(json.dumps({'errors':'worker disable'}),content_type='application/json')
+                return HttpResponse(json.dumps({'errors': 'worker disable'}), content_type='application/json')
             name = request.POST.get('name') if 'name' in request.POST else ""
             method = request.POST.get('value') if 'value' in request.POST else 'Unknow'
             items = Item.objects.filter(name=name,
-                                user=queries.get_user(url))
+                                        user=queries.get_user(url))
             if items.exists():
-                item=items[0]
+                item = items[0]
                 sale = Sale(
                     item=item,
                     volume=1,
                     creater_id=worker.id,
-                    create_time=timezone.now(),
+                    create_time=datetime.now(),
                     editer_id=worker.id,
-                    edit_time=timezone.now())
+                    edit_time=datetime.now())
                 sale.save()
-                result = salelist(request,item)
+                result = salelist(request, item)
             else:
-                result = {'fail':'norecorded'}
+                result = {'fail': 'norecorded'}
             data = json.dumps(result)
             return HttpResponse(data, content_type='application/json')
         else:
@@ -118,24 +123,25 @@ def AjaxSaleView(request,url):
     else:
         raise Http404
 
-def salelist(request,item):
-    worker =queries.get_worker(
-            url=request.session['url'],
-            username=request.session['worker'])
+
+def salelist(request, item):
+    worker = queries.get_worker(
+        url=request.session['url'],
+        username=request.session['worker'])
     sale_1 = Sale.objects.filter(
-                creater_id=worker.id,
-                create_time__lt=worker.date_log
-                )
+        creater_id=worker.id,
+        create_time__lt=worker.date_log
+    )
     sale_2 = Sale.objects.filter(
-                creater_id=worker.id
-                )
+        creater_id=worker.id
+    )
     first = sale_1.aggregate(Sum('volume'))['volume__sum'] if sale_1.exists() else 0
-    now   = sale_2.aggregate(Sum('volume'))['volume__sum'] if sale_2.exists() else 0
-    print(sale_1.count(),' sale1 count',worker.id)
+    now = sale_2.aggregate(Sum('volume'))['volume__sum'] if sale_2.exists() else 0
+    print(sale_1.count(), ' sale1 count', worker.id)
     return {
-        'id':item.id,
-        'name':item.name,
-        'first':first,
-        'now':now,
-        'sale':(now-first)
+        'id': item.id,
+        'name': item.name,
+        'first': first,
+        'now': now,
+        'sale': (now - first)
     }
